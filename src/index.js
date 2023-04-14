2b5ced0bc250e039b36ef705a4f6ab12367a39bf
snail15 <jungs@uchicago.edu>
2023-04-14 16:38:28 -0700

    firebase

--- src/index.js
+++ src/index.js
@@ -2,12 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
+import AllProducts from './pages/AllProducts';
+import NotFound from './pages/NotFound';
+import NewProduct from './pages/NewProduct';
+import ProductDetail from './pages/ProductDetail';
+import MyCart from './pages/MyCart';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/products', element: <AllProducts /> },
+      { path: '/products/new', element: <NewProduct /> },
+      { path: '/products/:id', element: <ProductDetail /> },
+      { path: '/carts', element: <MyCart /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
